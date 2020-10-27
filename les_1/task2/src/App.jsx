c9a88c0289400846a888b3749e0c876260ceab86
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-10-27 18:29:28 +0200

    Context react

---
+++ les_1/task2/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import { UserContext } from "./user-context";
+
+class App extends React.Component {
+  state = {
+    userData: {
+      name: 'Nikola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001'
+    },
+  }
+
+  render() {
+    return (
+      <UserContext.Provider value={this.state.userData}>
+        <Header />
+      </UserContext.Provider>
+    );
+  }
+}
+
+export default App;
+// export const Data = React.createContext(userData);
