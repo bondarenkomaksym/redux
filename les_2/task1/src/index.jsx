2b3bf4cc268bd30f654aa30fdcde3f7f40ca0b4b
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-10-28 11:56:38 +0200

    Счетчик - Redux

---
+++ les_2/task1/src/index.jsx
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  }
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT
+  }
+};
+
+//состояние должно быть неизменным
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+export const store = createStore(counterReducer);
+
