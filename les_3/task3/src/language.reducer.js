ba631f5fe95926b65124d5134a2943d966e1bc84
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-11-01 19:15:27 +0200

    3

--- les_3/task3/src/language.reducer.js
+++ les_3/task3/src/language.reducer.js
@@ -4,7 +4,7 @@ import { SET_LANGUAGE } from './language.actions';
 //   language: 'en',
 // }
 
-export const languageReducer = ((state = "en", action) => {
+export const languageReducer = (state = "en", action) => {
   switch (action.type) {
     case SET_LANGUAGE:
       return action.payload.language
