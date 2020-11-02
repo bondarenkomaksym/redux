02abf5de64d803680e748d03ef0f970eadb649b8
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-11-02 10:13:57 +0200

    React Redux Counter

---
+++ les_4/task1/src/components/Counter.jsx
@@ -0,0 +1,47 @@
+// import React, { useState } from "react";
+import React from "react";
+import { connect } from "react-redux";
+import { increment, decrement, reset } from '../counter.actions';
+
+
+const Counter = ({ counter, incr, decr, res }) => {
+
+  // const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="counter">
+      <button className="counter__button"
+        onClick={decr}
+      >
+        -
+    </button>
+      <span className="counter__value" onClick={res}>
+        {counter}
+      </span>
+      <button className="counter__button"
+        onClick={incr}
+      >
+        +
+    </button>
+    </div>
+  )
+}
+
+const mapState = state => {
+  return {
+    counter: state
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    incr: () => dispatch(increment()),
+    decr: () => dispatch(decrement()),
+    res: () => dispatch(reset()),
+  }
+}
+
+
+const connector = connect(mapState, mapDispatch);
+
+export default connector(Counter);
