43094fe8792f6d5d2a2cc3125e02e7eb15b9b661
bondarenkomaksym <bondarenkomaksym@users.noreply.github.com>
2020-11-05 13:58:56 +0200

    Redux loger middleware - part 2

---
+++ les_6/task2/src/users/users.actions.js
@@ -0,0 +1,18 @@
+export const SHOW_SPINNER = 'SHOW_SPINNER';
+export const USER_DATA_RECIEVED = 'USER_DATA_RECIEVED';
+
+export const showSpinner = () => {
+  return {
+    type: SHOW_SPINNER
+  }
+}
+
+export const userDataRecieved = (userData) => {
+  return {
+    type: USER_DATA_RECIEVED,
+    payload: {
+      userData
+    }
+  }
+}
+
